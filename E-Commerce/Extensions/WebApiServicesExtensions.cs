7e1fa46a2327b5498c6cbdac833e739cedbea38b
Mohamed Khaled <amer31824@gmail.com>
2025-11-09 23:29:31 +0200

    Part 8 Add Authorization to Swagger Doc

--- E-Commerce/Extensions/WebApiServicesExtensions.cs
+++ E-Commerce/Extensions/WebApiServicesExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using E_Commerce.Factories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 namespace E_Commerce.Extensions
 {
@@ -13,11 +14,43 @@ namespace E_Commerce.Extensions
                 options.InvalidModelStateResponseFactory =
                 ApiResponseFactory.CustomValidationErrorResponse;
             });
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
-            
+
+            services.ConfigureSwagger();
             return services;
         }
+
+        #region Part 8 Add Authorization to Swagger Doc
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(option =>
+            {
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+            }
+           });
+            });
+            return services;
+        } 
+        #endregion
     }
 }
