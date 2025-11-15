f047de7dba1ed35c2b948a4b1fbc66cb506e59df
Mohamed Khaled <amer31824@gmail.com>
2025-11-15 20:47:24 +0200

    Part 3 IOrderService & Dtos

---
+++ Shared/Dtos/OrderModule/OrderResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dtos.OrderModule
+{
+    public record  OrderResult
+    {
+        public Guid Id { get; init; }
+        public string UserEmail { get; init; } = string.Empty;
+        public AddressDto ShippingAddress { get; init; }
+
+        public ICollection<OrderItemDto> OrderItems { get; init; } = new List<OrderItemDto>();
+
+
+        public string PaymentStatus { get; init; } 
+        public string DeliveryMethod { get; init; }
+        public int? DeliveryMethodId { get; init; }
+
+        // OrderItem.Price * OrderItem.Quantity
+        // Total == Subtotal + DeliveryMethod.Price [ Derieved Atribute ] --> DTO OR Mapping Profile
+        public decimal Subtotal { get; init; }
+        public string PaymentIntentId { get; init; } = string.Empty;
+
+        public DateTimeOffset OrderDate { get; init; } = DateTimeOffset.Now;
+
+        public decimal Total { get; init; }
+    }
+}
