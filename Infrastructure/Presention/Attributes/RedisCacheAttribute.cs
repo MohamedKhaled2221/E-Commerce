8fc06034b823bc57cd694e73cc45ad0bddbb55af
Mohamed Khaled <amer31824@gmail.com>
2025-11-30 14:27:30 +0200

    Part 4 Cache Attribute

--- Infrastructure/Presention/Attributes/RedisCacheAttribute.cs
+++ Infrastructure/Presention/Attributes/RedisCacheAttribute.cs
@@ -11,41 +11,42 @@ using Services.Abstraction.Contracts;
 
 namespace Presention.Attributes
 {
-    internal class RedisCacheAttribute(int durationInSecondes =60) : ActionFilterAttribute
+    #region Part 4 Cache Attribute
+    internal class RedisCacheAttribute(int durationInSecondes = 60) : ActionFilterAttribute
     {
         public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-           var cacheService = context.HttpContext.RequestServices.GetRequiredService<IServiceManager>().CacheService;
+            var cacheService = context.HttpContext.RequestServices.GetRequiredService<IServiceManager>().CacheService;
             // Data Cached Or Not ==> Cache Key
 
             string cacheKey = GenerateCacheKey(context.HttpContext.Request);
 
-            var result =await cacheService.GetCachedValueAsync(cacheKey);
+            var result = await cacheService.GetCachedValueAsync(cacheKey);
 
             // Data Already Cached [ Return Response WithOut Entering The Endpoint ]
             if (result is not null)
             {
                 // Return From Cache
-               context.Result = new ContentResult()
-               {
-                   Content = result,
-                   ContentType = "Application/Json",
-                   StatusCode = StatusCodes.Status200OK
-               };
+                context.Result = new ContentResult()
+                {
+                    Content = result,
+                    ContentType = "Application/Json",
+                    StatusCode = StatusCodes.Status200OK
+                };
                 return;
             }
 
             var contextResult = await next.Invoke();
 
-            if (contextResult.Result is OkObjectResult okObject) 
+            if (contextResult.Result is OkObjectResult okObject)
                 // Store Data In Cache
-                await cacheService.SetCacheValueAsync(cacheKey, okObject , TimeSpan.FromMinutes(durationInSecondes));
+                await cacheService.SetCacheValueAsync(cacheKey, okObject, TimeSpan.FromMinutes(durationInSecondes));
 
-            }
+        }
 
         private string GenerateCacheKey(HttpRequest request)
         {
-           var keyBuilder = new StringBuilder();
+            var keyBuilder = new StringBuilder();
             // Request.Path ==> /api/products
 
             // Request.Query ==> ?brandId=1&typeId=2&pageIndex=1&pageSize=10&sort=priceAsc
@@ -53,11 +54,12 @@ namespace Presention.Attributes
 
             keyBuilder.Append(request.Path);
 
-            foreach (var item in request.Query.OrderBy(q=>q.Key))
+            foreach (var item in request.Query.OrderBy(q => q.Key))
             {
                 keyBuilder.Append($"|{item.Key}-{item.Value}");
             }
             return keyBuilder.ToString();
         }
-    }
+    } 
+    #endregion
 }
